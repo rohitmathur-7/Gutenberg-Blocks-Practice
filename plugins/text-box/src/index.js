a29ed1107ff45c07d62265caf151ea8da47cfb99
rohitmathur-7 <rohitmathur.mathur.6@gmail.com>
2024-02-27 18:47:38 +0530

    Added deprecations and transformations for block

--- plugins/text-box/src/index.js
+++ plugins/text-box/src/index.js
@@ -1,17 +1,66 @@
-import { registerBlockType } from "@wordpress/blocks";
-import "./style.scss";
-import Edit from "./edit";
-import save from "./save";
-import metadata from "./block.json";
+import { registerBlockType, createBlock } from '@wordpress/blocks';
+import './style.scss';
+import Edit from './edit';
+import save from './save';
+import metadata from './block.json';
 
 registerBlockType(metadata.name, {
-	/**
-	 * @see ./edit.js
-	 */
 	edit: Edit,
-
-	/**
-	 * @see ./save.js
-	 */
 	save,
+	variations: [
+		{
+			name: 'my-blocks/gradient-text-box',
+			title: 'Gradient Text Box',
+			icon: 'superhero-alt',
+			attributes: {
+				gradient: 'red-to-blue',
+			},
+		},
+	],
+	transforms: {
+		from: [
+			{
+				type: 'block',
+				blocks: ['core/paragraph'],
+				transform: ({ content, align }) => {
+					return createBlock('my-blocks/text-box', {
+						text: content,
+						alignment: align,
+					});
+				},
+			},
+			{
+				type: 'enter',
+				regExp: /textbox/i,
+				transform: () => {
+					return createBlock('my-blocks/text-box', {
+						shadow: true,
+						gradient: 'red-to-blue',
+					});
+				},
+			},
+			{
+				type: 'prefix',
+				prefix: 'textbox',
+				transform: () => {
+					return createBlock('my-blocks/text-box');
+				},
+			},
+		],
+		to: [
+			{
+				type: 'block',
+				blocks: ['core/paragraph'],
+				isMatch: ({ text }) => {
+					return text ? true : false;
+				},
+				transform: ({ text, alignment }) => {
+					return createBlock('core/paragraph', {
+						content: text,
+						align: alignment,
+					});
+				},
+			},
+		],
+	},
 });
