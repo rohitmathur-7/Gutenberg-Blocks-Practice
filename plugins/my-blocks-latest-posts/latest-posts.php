3b0dff02faeb62aedc224e7f998db731122e78ba
rohitmathur-7 <rohitmathur.mathur.6@gmail.com>
2024-03-05 10:18:46 +0530

    Showed dynamic content on frontend, editor. Styled the Latest Post block in frontend and editor. Added option to display featured image

--- plugins/my-blocks-latest-posts/latest-posts.php
+++ plugins/my-blocks-latest-posts/latest-posts.php
@@ -17,6 +17,38 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly.
 }
 
+function my_block_render_latest_posts_block( $attributes ) {
+	$args = [ 
+		'posts_per_page' => $attributes['numberOfPosts'],
+		'post_status'    => 'publish'
+	];
+
+	$recent_posts = get_posts( $args );
+
+	$posts = '<ul ' . get_block_wrapper_attributes() . '>';
+	foreach ( $recent_posts as $post ) {
+		$title     = get_the_title( $post );
+		$title     = $title ? $title : __( "No Title", "latest-posts" );
+		$permalink = get_permalink( $post );
+		$excerpt   = get_the_excerpt( $post );
+		$posts .= '<li>';
+		if ( $attributes["displayFeaturedImage"] && has_post_thumbnail( $post ) ) {
+			$posts .= get_the_post_thumbnail( $post, 'medium' );
+		}
+		$posts .= '<h5><a href="' . esc_url( $permalink ) . '">' . $title . '</a></h5>';
+		$posts .= '<time datatime="' . esc_attr( get_the_date( 'c', $post ) ) . '">' . esc_html( get_the_date( '', $post ) ) . '</time>';
+		if ( ! empty( $excerpt ) ) {
+			$posts .= '<p>' . $excerpt . '</p>';
+		}
+		$posts .= '</li>';
+	}
+	$posts .= '</ul>';
+
+
+	return $posts;
+}
+
+
 /**
  * Registers the block using the metadata loaded from the `block.json` file.
  * Behind the scenes, it registers also all assets so they can be enqueued
@@ -25,6 +57,8 @@ if ( ! defined( 'ABSPATH' ) ) {
  * @see https://developer.wordpress.org/reference/functions/register_block_type/
  */
 function my_blocks_latest_posts_block_init() {
-	register_block_type( __DIR__ . '/build' );
+	register_block_type( __DIR__ . '/build', array(
+		'render_callback' => 'my_block_render_latest_posts_block'
+	) );
 }
 add_action( 'init', 'my_blocks_latest_posts_block_init' );
--- plugins/my-blocks-latest-posts/src/edit.js
+++ plugins/my-blocks-latest-posts/src/edit.js
@@ -1,38 +1,84 @@
-/**
- * Retrieves the translation of text.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-i18n/
- */
 import { __ } from '@wordpress/i18n';
-
-/**
- * React hook that is used to mark the block wrapper element.
- * It provides all the necessary props like the class name.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/packages/packages-block-editor/#useblockprops
- */
-import { useBlockProps } from '@wordpress/block-editor';
-
-/**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * Those files can contain any CSS code that gets applied to the editor.
- *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
- */
+import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
 import './editor.scss';
+import { useSelect } from '@wordpress/data';
+import { RawHTML } from '@wordpress/element';
+import { format, dateI18n, getSettings } from '@wordpress/date';
+import { PanelBody, ToggleControl, QueryControls } from '@wordpress/components';
 
-/**
- * The edit function describes the structure of your block in the context of the
- * editor. This represents what the editor will render when the block is used.
- *
- * @see https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/#edit
- *
- * @return {Element} Element to render.
- */
-export default function Edit() {
+export default function Edit({ attributes, setAttributes }) {
+	const { numberOfPosts, displayFeaturedImage } = attributes;
+
+	const posts = useSelect(
+		(select) => {
+			return select('core').getEntityRecords('postType', 'post', {
+				per_page: numberOfPosts,
+				_embed: true,
+			});
+		},
+		[numberOfPosts]
+	);
+
+	const onChangeDisplayFeaturedImage = (value) => {
+		setAttributes({ displayFeaturedImage: value });
+	};
+
 	return (
-		<p {...useBlockProps()}>
-			{__('Latest Posts – hello from the editor!', 'latest-posts')}
-		</p>
+		<>
+			<InspectorControls>
+				<PanelBody>
+					<ToggleControl
+						label={__('Display Featured Image', 'latest-posts')}
+						checked={displayFeaturedImage}
+						onChange={onChangeDisplayFeaturedImage}
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<ul {...useBlockProps()}>
+				{posts &&
+					posts.map((post) => {
+						const featuredImage =
+							post._embedded &&
+							post._embedded['wp:featuredmedia'] &&
+							post._embedded['wp:featuredmedia'].length > 0 &&
+							post._embedded['wp:featuredmedia'][0];
+						return (
+							<li key={post.id}>
+								{displayFeaturedImage && featuredImage && (
+									<img
+										src={
+											featuredImage.media_details.sizes
+												.medium.source_url
+										}
+										alt={featuredImage.alt_text}
+									/>
+								)}
+								<h5>
+									<a href={post.link}>
+										{post.title.rendered ? (
+											<RawHTML>
+												{post.title.rendered}
+											</RawHTML>
+										) : (
+											__('(No Title)', 'latest-posts')
+										)}
+									</a>
+								</h5>
+								{post.date_gmt && (
+									<time dateTime={format('c', post.date_gmt)}>
+										{dateI18n(
+											getSettings().formats.date,
+											post.date_gmt
+										)}
+									</time>
+								)}
+								{post.excerpt.rendered && (
+									<RawHTML>{post.excerpt.rendered}</RawHTML>
+								)}
+							</li>
+						);
+					})}
+			</ul>
+		</>
 	);
 }
